94e067ea0c0a9174f686112a34b1c7202fd992c3
Enoque Jr <enoquejrr@gmail.com>
2025-03-14 16:18:28 -0300

    Implementação da depedência JPA e criação do FuncionarioModel como entidade

---
+++ src/main/java/com/sonda/CadastroFuncionario/FuncionarioModel.java
@@ -0,0 +1,55 @@
+package com.sonda.CadastroFuncionario;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class FuncionarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private int telefone;
+
+    public FuncionarioModel(String nome, String email, int idade, int telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+}
