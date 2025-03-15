02707b98b3f49867825d852eef73d0334c882ee8
Enoque Jr <enoquejrr@gmail.com>
2025-03-15 14:12:34 -0300

    Alteração do pom.xlm para download da dependência do Lombock. Implementação do @NoArgsConstructor, @AllArgsConstructor e @Data em FuncionarioModel e AtividadeModel.

--- src/main/java/com/sonda/CadastroFuncionario/Atividades/AtividadesModel.java
+++ src/main/java/com/sonda/CadastroFuncionario/Atividades/AtividadesModel.java
@@ -2,11 +2,17 @@ package com.sonda.CadastroFuncionario.Atividades;
 
 import com.sonda.CadastroFuncionario.Funcionario.FuncionarioModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_atividades")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AtividadesModel {
 
     @Id
--- src/main/java/com/sonda/CadastroFuncionario/Funcionario/FuncionarioModel.java
+++ src/main/java/com/sonda/CadastroFuncionario/Funcionario/FuncionarioModel.java
@@ -1,11 +1,17 @@
 package com.sonda.CadastroFuncionario.Funcionario;
 import com.sonda.CadastroFuncionario.Atividades.AtividadesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FuncionarioModel {
 
     @Id
@@ -21,45 +27,4 @@ public class FuncionarioModel {
     @JoinColumn(name = "atividades_id")
     private AtividadesModel atividades;
 
-    public FuncionarioModel() {
-    }
-
-    public FuncionarioModel(String nome, String email, int idade, int telefone) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.telefone = telefone;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public int getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(int telefone) {
-        this.telefone = telefone;
-    }
 }
